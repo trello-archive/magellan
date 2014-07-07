97488c22747c1300067c4ead18b65bc86e43472d
Ian Henry <ianthehenry@gmail.com>
2014-07-07 16:39:40 -0400

    replace MAGRouteComponent with a block :facepalm:

--- MAGRoute.m
+++ MAGRoute.m
@@ -8,59 +8,25 @@
 
 #import "MAGRoute.h"
 
+typedef NSString *(^MAGRouteComponent)(id obj);
+
 @interface MAGRoute ()
+
 @property (nonatomic, strong) NSArray *components;
-@end
-
-@protocol MAGRouteComponent <NSObject>
-- (NSString *)route:(id)object;
-@end
-
-@interface MAGRouteLiteralComponent : NSObject <MAGRouteComponent>
-@property (nonatomic, copy) NSString *string;
-+ (instancetype)componentWithString:(NSString *)string;
-@end
-
-@interface MAGRouteKeyPathComponent : NSObject <MAGRouteComponent>
-@property (nonatomic, copy) NSString *keyPath;
-+ (instancetype)componentWithKeyPath:(NSString *)keyPath;
-@end
-
-@implementation MAGRouteLiteralComponent
-
-+ (instancetype)componentWithString:(NSString *)string {
-    MAGRouteLiteralComponent *this = [[self alloc] init];
-    this.string = string;
-    return this;
-}
-
-- (NSString *)route:(id)object {
-    return self.string;
-}
-
-@end
-
-@implementation MAGRouteKeyPathComponent
-
-+ (instancetype)componentWithKeyPath:(NSString *)keyPath {
-    MAGRouteKeyPathComponent *this = [[self alloc] init];
-    this.keyPath = keyPath;
-    return this;
-}
-
-- (NSString *)route:(id)object {
-    return [object valueForKeyPath:self.keyPath];
-}
 
 @end
 
 @implementation MAGRoute
 
-+ (id <MAGRouteComponent>)routeComponentWithFormat:(NSString *)format {
++ (MAGRouteComponent)routeComponentWithFormat:(NSString *)format {
     if ([format hasPrefix:@":"]) {
-        return [MAGRouteKeyPathComponent componentWithKeyPath:[format substringFromIndex:1]];
+        return ^(id obj) {
+            return [obj valueForKeyPath:[format substringFromIndex:1]];
+        };
     } else {
-        return [MAGRouteLiteralComponent componentWithString:format];
+        return ^(id obj) {
+            return format;
+        };
     }
 }
 
@@ -79,8 +45,8 @@
 
 - (NSString *)route:(id)object {
     NSMutableArray *result = [[NSMutableArray alloc] init];
-    for (id <MAGRouteComponent> component in self.components) {
-        [result addObject:[component route:object]];
+    for (MAGRouteComponent component in self.components) {
+        [result addObject:component(object)];
     }
     return [result componentsJoinedByString:@"/"];
 }
